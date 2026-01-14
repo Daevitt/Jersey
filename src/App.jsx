e092ae18da444ee3d2389f08aab10400d19c70b4
Daevitt <david.eternalsgaming@gmail.com>
2026-01-14 14:18:28 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
-import WalletProvider from './walletProvide.jsx'
+import { useEffect, useState } from 'react'
 
 import Homepage from './Pages/Homepage'
 import Home from './Pages/Home'
@@ -14,26 +14,44 @@ import BillingPage2 from './Pages/BillingPage2'
 import About from './Pages/About'
 
 function App() {
-  return (
-    <WalletProvider>
-      <div className='w-full min-h-screen'>
-        <Routes>
-          <Route path='/' element={<Homepage />}>
-            <Route index element={<Home />} />
-            <Route path='Events' element={<Events />} />
-            <Route path='Signup' element={<SignUp />} />
-            <Route path='Login' element={<Login />} />
-            <Route path='CreateEvent' element={<CreateEvent />} />
-            <Route path='Billing/:id' element={<BillingPage2 />} />
-            <Route path='Event/:id' element={<Event />} />
-            <Route path='About' element={<About />} />
-            <Route path='Payment/:id' element={<Payment2 />} />
-            <Route path='*' element={<NotFound />} />
-          </Route>
-        </Routes>
-      </div>
-    </WalletProvider>
+  const [WalletProvider, setWalletProvider] = useState(null)
+
+  useEffect(() => {
+    // Carga WalletProvider SOLO en el navegador
+    import('./walletProvide.jsx')
+      .then((mod) => {
+        setWalletProvider(() => mod.default)
+      })
+      .catch((err) => {
+        console.error('Error loading WalletProvider', err)
+      })
+  }, [])
+
+  const AppContent = (
+    <div className='w-full min-h-screen'>
+      <Routes>
+        <Route path='/' element={<Homepage />}>
+          <Route index element={<Home />} />
+          <Route path='Events' element={<Events />} />
+          <Route path='Signup' element={<SignUp />} />
+          <Route path='Login' element={<Login />} />
+          <Route path='CreateEvent' element={<CreateEvent />} />
+          <Route path='Billing/:id' element={<BillingPage2 />} />
+          <Route path='Event/:id' element={<Event />} />
+          <Route path='About' element={<About />} />
+          <Route path='Payment/:id' element={<Payment2 />} />
+          <Route path='*' element={<NotFound />} />
+        </Route>
+      </Routes>
+    </div>
   )
+
+  // Mientras WalletProvider no está cargado, renderizamos igual
+  if (!WalletProvider) {
+    return AppContent
+  }
+
+  return <WalletProvider>{AppContent}</WalletProvider>
 }
 
 export default App

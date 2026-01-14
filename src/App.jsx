4abf4f559bcf60bb0e4a45871659760ff0cc4c63
Daevitt <david.eternalsgaming@gmail.com>
2026-01-14 14:15:31 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { Routes,Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+import WalletProvider from './walletProvide.jsx'
+
 import Homepage from './Pages/Homepage'
 import Home from './Pages/Home'
 import Events from './Pages/Events'
 import SignUp from './Pages/SignUp'
 import Login from './Pages/Login'
-// import BillingPage from './Pages/BillingPage'
 import NotFound from './Pages/NotFound'
 import Event from './Pages/Event'
 import Payment2 from './Pages/Payment2'
@@ -14,23 +15,25 @@ import About from './Pages/About'
 
 function App() {
   return (
-    <div className='w-full min-h-screen'>
-      <Routes>
-        <Route path = '/' element = {<Homepage/>}>
-          <Route index element = {<Home/>}/>
-          <Route path = '/Events' element = {<Events/>}/>
-          <Route path = '/Signup' element = {<SignUp/>}/>
-          <Route path = '/Login' element = {<Login/>}/>
-          <Route path = '/CreateEvent' element = {<CreateEvent/>}/>
-          <Route path = '/Billing/:id' element = {<BillingPage2/>}/>
-          <Route path = '/Event/:id' element = {<Event/>}/>
-          <Route path = '/About' element = {<About/>}/>
-          <Route path = '/Payment/:id' element = {<Payment2/>}/>
-          <Route path = '*' element = {<NotFound/>}/>
-        </Route>
-      </Routes>
-    </div>
+    <WalletProvider>
+      <div className='w-full min-h-screen'>
+        <Routes>
+          <Route path='/' element={<Homepage />}>
+            <Route index element={<Home />} />
+            <Route path='Events' element={<Events />} />
+            <Route path='Signup' element={<SignUp />} />
+            <Route path='Login' element={<Login />} />
+            <Route path='CreateEvent' element={<CreateEvent />} />
+            <Route path='Billing/:id' element={<BillingPage2 />} />
+            <Route path='Event/:id' element={<Event />} />
+            <Route path='About' element={<About />} />
+            <Route path='Payment/:id' element={<Payment2 />} />
+            <Route path='*' element={<NotFound />} />
+          </Route>
+        </Routes>
+      </div>
+    </WalletProvider>
   )
 }
 
-export default App;
+export default App

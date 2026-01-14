77199b0653d850917b1d16bf7502c20097115007
Daevitt <david.eternalsgaming@gmail.com>
2026-01-14 14:44:34 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { Toaster } from 'react-hot-toast'
-import App from './App.jsx'
-import './index.css'
-import AppContextProvider from './Context/AppContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <AppContextProvider>
-      <Toaster />
-      <App />
-    </AppContextProvider>
-  </BrowserRouter>
+  <h1 style={{ color: 'red' }}>REACT FUNCIONA</h1>
 )
